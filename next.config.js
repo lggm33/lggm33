35ed4c2c6f0c7a6e51113a9a2ef9b1451b08d0be
lggm <lgomez@twilio.com>
2024-09-25 22:52:35 -0600

    cambio de next.config to export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true, // Necesario si estás utilizando el módulo de imágenes de Next.js
+    },
+    // basePath: '/REPO-NAME', // El nombre de tu repositorio (esto es importante para GitHub Pages)
+  };
+  
+  module.exports = nextConfig;
+  
